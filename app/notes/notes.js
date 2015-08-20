9fca65d5d253afba5d47c1e20b453f8338468205
Kaycie Neeley <kaycieneeley@gmail.com>
2015-08-20 14:03:49 -0400

    Adds Note to sidebar

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -1,14 +1,30 @@
 'use strict';
 
+var nevernoteBasePath = 'https://nevernote-1150.herokuapp.com/api/v1/';
+var apiKey = '$2a$10$qSiC0G4sQW3UljA8W5bAr.6RU7pWAq1OJ6DnePavjdxICHbN8ZcMi';
+
 angular.module('notely.notes', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider){
-  $routeProvider.when('/notes',{
-    templateUrl: 'notes/notes.html',
-    controller: 'NotesController' //capitalize controllers
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/notes', {
+    templateUrl: 'notes/notes.html'
   });
 }])
 
-.controller('NotesController', [function(){
+.controller('NotesController', ['$scope', '$http', function($scope, $http) {
+  $scope.note = {};
 
+  $http.get(nevernoteBasePath + 'notes?api_key=' + apiKey)
+    .success(function(notesData) {
+      $scope.notes = notesData;
+    });
+
+  $scope.commit = function() {
+    $http.post(nevernoteBasePath + 'notes', {
+      api_key: apiKey,
+      note: $scope.note
+    }).success(function(newNoteData) {
+      $scope.notes.unshift(newNoteData.note);
+    });
+  };
 }]);
